ca291e98780cc9a9fbeb5165411421a4b08e996c
gabrielMatosBoubee <gm.boubee@gmail.com>
2024-05-10 09:50:36 -0300

    feat: add backgroundColor in footer

--- components/footerV2/footer.tsx
+++ components/footerV2/footer.tsx
@@ -6,21 +6,24 @@ export interface Props {
         text: string;
         position: "center" | "left" | "right"
     }
-
     credits: string;
+    /** @format color-input */
+    backgroundColor: string;
 }
 
-function footer({ title, credits }: Props) {
+function footer({ title, credits, backgroundColor }: Props) {
     const Divider = () => <div class="w-full h-px bg-[#0D1717]" />
     return (
-        <footer class="container h-[241px] py-6 px-16 flex flex-col gap-8">
-            <p class="text-2xl text-[#0D1717] font-bold" style={clx(title.position)}>{title.text}</p>
-            <Divider />
-            <div class="flex items-center justify-between h-[35px]">
-                <p class="text-sm font-normal text-[#0D1717]">{credits}</p>
-                <Icon id="poweredBy" width={159} height={35} />
-            </div>
-        </footer>
+        <div class="w-full" style={{ backgroundColor }}>
+            <footer class="container h-[241px] py-6 px-16 flex flex-col gap-8">
+                <p class="text-2xl text-[#0D1717] font-bold" style={clx(title.position)}>{title.text}</p>
+                <Divider />
+                <div class="flex items-center justify-between h-[35px]">
+                    <p class="text-sm font-normal text-[#0D1717]">{credits}</p>
+                    <Icon id="poweredBy" width={159} height={35} />
+                </div>
+            </footer>
+        </div>
     );
 }
 
